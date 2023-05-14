7d19cf614c655693a3e1ce18c7ca01f1390fe9f9
abraham-ship <bettabraham11@gmail.com>
2023-05-13 22:58:02 -0700

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if not sentence:
-        return None
+        sentence[0] = None
     else:
         length = len(sentence)
         first = sentence[0]
