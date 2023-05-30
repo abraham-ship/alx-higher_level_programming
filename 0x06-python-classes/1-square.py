1b7fea31756351c530992c6978f7e9202f1accab
abraham-ship <bettabraham11@gmail.com>
2023-05-30 02:17:08 -0700

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Square:
+    """Class that defines a square."""
+
+    def __init__(self, size):
+        """Initialize square.
+
+        Args:
+            size (int): Private instance attribute.
+
+        """
+        self.__size = size
