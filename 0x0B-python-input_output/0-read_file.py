00ae9499861bf53320a3c6d026de6cf397366dbb
abraham-ship <bettabraham11@gmail.com>
2023-06-13 02:37:12 -0700

    added documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
+""" read text file"""
+
+
 def read_file(filename=""):
+    """
+    a function that reads a text file (UTF8) and prints it to stdout:
+        Args:
+            filename: file to be read
+    """
     with open(filename, mode="r", encoding="utf-8") as file:
         print(file.read())
