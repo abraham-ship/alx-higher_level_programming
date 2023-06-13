79be483e31201ad6ffd341de56e4758c0b98519d
abraham-ship <bettabraham11@gmail.com>
2023-06-13 11:13:30 -0700

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,9 +2,9 @@
 
 """Load, add, save"""
 import sys
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').
+    load_from_json_file
 
 def add_arguments_to_list(arguments):
     """
