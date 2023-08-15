01b0d372926ecd97225b757c19b43d5f42fec3b4
abraham-ship <abuzsein3@gmail.com>
2023-08-15 09:53:16 -0700

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const SquareBase = require('./5-square');
+
+class Square extends SquareBase {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
