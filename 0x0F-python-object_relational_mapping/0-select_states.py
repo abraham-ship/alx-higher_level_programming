922caff182812d95e504ae16933750c1f7d0625b
abraham-ship <bettabraham11@gmail.com>
2023-08-18 02:58:18 -0700

    hot fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user="root",
-                           passwd="root", db="hbtn_0e_0_usa", charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306, user="mysql_username",
+                           passwd="mysql_password", db="database_name")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id")
     query_rows = cur.fetchall()
