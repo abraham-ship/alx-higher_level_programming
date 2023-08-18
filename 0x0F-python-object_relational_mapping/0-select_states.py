4045c464368a6695b4856838d4a31bf1e2a87278
abraham-ship <bettabraham11@gmail.com>
2023-08-18 02:53:19 -0700

    hot fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user="root",
                            passwd="root", db="hbtn_0e_0_usa", charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states ORDER BY states.id")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
