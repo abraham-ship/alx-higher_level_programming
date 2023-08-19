bca4f19ce674449cfdbec8dd03adcd5836de276d
abraham-ship <bettabraham11@gmail.com>
2023-08-18 23:12:35 -0700

    Contains `a`

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -9,13 +9,14 @@ import sys
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    # create custom session object class from database engine
+
     Session = sessionmaker(bind=engine)
-    # create instance of new custom session class
     session = Session()
+
     states = session.query(State)\
                     .filter(State.name.contains('a'))\
                     .order_by(State.id)
+
     if (states is not None):
         for state in states:
             print('{}: {}'.format(state.id, state.name))
