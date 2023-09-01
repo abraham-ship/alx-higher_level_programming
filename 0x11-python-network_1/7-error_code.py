e7634394740d61429677824d4ba3989f1726ac63
abraham-ship <bettabraham11@gmail.com>
2023-09-01 05:09:36 -0700

    Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+'''send request to URL and display body of the response'''
+if __name__ == '__main__':
+    import requests
+    import sys
+    from requests.exceptions import HTTPError
+    try:
+        url = sys.argv[1]
+        r = requests.get(url)
+
+    except HTTPError as e:
+        print(f"Error code: {e}")
