bd31a3a47462c68c6d66ac84cb63a02d0194836d
abraham-ship <bettabraham11@gmail.com>
2023-09-01 05:28:28 -0700

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,10 +5,9 @@ if __name__ == '__main__':
     import requests
     import sys
     from requests.exceptions import HTTPError
-    try:
-        url = sys.argv[1]
-        r = requests.get(url)
-        r.raise_for_status()
+    url = sys.argv[1]
+    r = requests.get(url)
+    if r.status_code >= '400':
+        print(f"Error code: {r.status_code}")
+    else:
         print(r.text)
-    except requests.exceptions.RequestException as e:
-        print(f"Error code: {response.status_code}")
