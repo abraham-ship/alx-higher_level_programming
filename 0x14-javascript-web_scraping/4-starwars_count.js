8a5d889ef1cc7a6fa99beb95a2556d2d6d28263b
abraham-ship <bettabraham11@gmail.com>
2023-09-26 08:30:09 -0700

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,19 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
+let count = 0;
 request.get(url, (err, response, body) => {
   if (err) throw err;
 
   const movieData = JSON.parse(body);
-  const id = 18;
-  const filmsWithWedgeAntilles = movieData.filter((film) =>
-    film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${id}/`)
-  );
-  const numberofmovies = filmsWithWedgeAntilles.length;
-  console.log(numberofmovies);
+  const characters = movieData.characters;
+  characters.forEach((characterUrl) => {
+    const characterId = characterUrl.split('/').slice(-2, -1)[0];
+    if (characterId === '18') {
+      count += 1;
+    }
+  });
+  console.log(count);
 });
